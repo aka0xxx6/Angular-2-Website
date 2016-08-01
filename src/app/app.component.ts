11ddb19b47570558537998d60c88ecb493d1e198
youchra <nemmo@gmx.de>
2016-08-01 12:55:44 +0200

    added twitter-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+
+import { AlertComponent } from 'ng2-bootstrap/ng2-bootstrap';
+
 import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [AlertComponent]
 })
 export class AppComponent { }
